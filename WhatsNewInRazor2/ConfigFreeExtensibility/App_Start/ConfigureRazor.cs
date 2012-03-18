6540c5bf63fd6297e5bbb960b25d2f7e98bbe493
Andrew Nurse <andrew@andrewnurse.net>
2012-03-17 17:14:25 -0700

    Initial Import of WhatsNewInRazor2 demo

---
+++ WhatsNewInRazor2/ConfigFreeExtensibility/App_Start/ConfigureRazor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages.Razor;
+using ConfigFreeExtensibility.Helpers;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(ConfigFreeExtensibility.App_Start.ConfigureRazor), "Start")]
+
+namespace ConfigFreeExtensibility.App_Start
+{
+    public static class ConfigureRazor
+    {
+        public static void Start()
+        {
+            RazorBuildProvider.CompilingPath += (sender, args) =>
+            {
+                args.Host.NamespaceImports.Add(typeof(MyHelpers).Namespace);
+            };
+        }
+    }
+}
---
+++ WhatsNewInRazor2/ConfigFreeExtensibility/Helpers/MyHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConfigFreeExtensibility.Helpers
+{
+    public static class MyHelpers
+    {
+        public static IHtmlString ExternalLink(this HtmlHelper self, string title, string url)
+        {
+            return new HtmlString(String.Format("<a class=\"ext\" href=\"{1}\">{0}</a>", title, url));
+        }
+    }
+}
